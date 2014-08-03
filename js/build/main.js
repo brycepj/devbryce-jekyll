ba95d18aa1582cf2e87e6d31a365774b38fee5bf
bryce <brycepj@gmail.com>
2014-08-03 19:32:26 -0400

    blank commit

--- js/build/main.js
+++ js/build/main.js
@@ -1,35 +1,5 @@
 $(document).ready(function(){
-
-
-//Masonry Configuration and Execution on Home Page
-
-(function(){
-   var blogHome = new MasonryConfig();
-	blogHome.initHome();
-
-
-
-
-})();
-
-
-function MasonryConfig() {
-
-	var $content = $('.content');
-
-	this.initHome = function(){
-		$content.masonry({
-			gutter:20,
-			isFitWidth:true,
-			transitionDuration:'0',
-			itemSelector:'.post-preview',
-			// isInitLayout: true
-		});
-	};
-
-}
-
-
-
-
+	
+	console.log('document ready');
+	
 });
